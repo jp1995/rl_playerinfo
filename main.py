166390d8f4b623f22950d3861afa23983aa22c03
jp1995 <Johanpoldaru@gmail.com>
2022-08-17 13:57:45 +0300

    Finished code extracting data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from webdriver.webdriver_conf import webdriver_conf
 from webdriver.updateWebdriver import updateWebdriver
+from tabulate import tabulate
 from time import sleep
 import json
 import os
@@ -16,14 +17,14 @@ class rl_playerinfo:
         self.gen_base_url = 'https://rocketleague.tracker.network/rocket-league/profile'
         self.storage = ''
 
-    def readDatafile(self):
+    def readNames(self):
         self.mainDict = {}
-        with open(self.plugDir+"names.txt", encoding='utf-8') as f:
+        with open(self.plugDir + "names.txt", encoding='utf-8') as f:
             lines = [line.rstrip() for line in f]
 
         return lines[0]
 
-    def transformData(self, namesstr: str):
+    def cleanNames(self, namesstr: str):
         if namesstr != self.storage:
             self.storage = namesstr
 
@@ -37,68 +38,73 @@ class rl_playerinfo:
 
             return self.mainDict
 
-    def request(self, dicty: dict):
-        print(dicty)
+    def handleData(self, dicty: dict):
+        #print(dicty)
+        headings = ['Name', '1v1', '2v2', '3v3', 'Wins', 'Reward level',
+                    'Influencer', 'Premium', 'Sussy', 'Country', 'url']
+
+        table = [headings]
+
         for name, platform in dicty.items():
             api_url = f'{self.api_base_url}/{platform}/{name}'
             gen_url = f'{self.gen_base_url}/{platform}/{name}/overview'
+            rankdict = {}
+            totaldata = []
+            totalprint = []
 
             api_resp = webdriver_conf(api_url).split(';">')[1].split('</pre>')[0]
             data = json.loads(api_resp)
-
-            print(json.dumps(data, indent=4))
+            # print(json.dumps(data, indent=4))
 
             clr_name = data['data']['platformInfo']['platformUserHandle']
-
-            # if 'Ranked Duel 1v1' in data['data']['segments'][1]:
-            #     ones_tier = data['data']['segments'][1]['stats']['tier']['metadata']['name']
-            #     ones_div = data['data']['segments'][1]['stats']['division']['metadata']['name']
-            #     twos_tier = data['data']['segments'][2]['stats']['tier']['metadata']['name']
-            #     twos_div = data['data']['segments'][2]['stats']['division']['metadata']['name']
-            #     threes_tier = data['data']['segments'][3]['stats']['tier']['metadata']['name']
-            #     threes_div = data['data']['segments'][3]['stats']['division']['metadata']['name']
-            # else:
-            #     ones_tier = data['data']['segments'][2]['stats']['tier']['metadata']['name']
-            #     ones_div = data['data']['segments'][2]['stats']['division']['metadata']['name']
-            #     twos_tier = data['data']['segments'][3]['stats']['tier']['metadata']['name']
-            #     twos_div = data['data']['segments'][3]['stats']['division']['metadata']['name']
-            #     threes_tier = data['data']['segments'][4]['stats']['tier']['metadata']['name']
-            #     threes_div = data['data']['segments'][4]['stats']['division']['metadata']['name']
-
-            ranks = []
-            for i in range(0, 3):
-                if 'Ranked Duel 1v1' in data['data']['segments'][i]:
-                    print('drue')
-                    tier = data['data']['segments'][i]['stats']['tier']['metadata']['name']
-                    div = data['data']['segments'][i]['stats']['division']['metadata']['name']
-                    ranks.append(f'{tier} {div}')
-                else:
-                    print('false')
-                    tier = data['data']['segments'][i+1]['stats']['tier']['metadata']['name']
-                    div = data['data']['segments'][i+1]['stats']['division']['metadata']['name']
-                    ranks.append(f'{tier} {div}')
-
-
-
-
-            wins = data['data']['segments'][0]['stats']['wins']['value']
+            premium = data['data']['userInfo']['isPremium']
             sussy = data['data']['userInfo']['isSuspicious']
-            print(f'{clr_name} -- 1v1: {ranks[0]}   |   2v2: {ranks[1]}   |   '
-                  f'3v3: {ranks[2]}   |   Wins: {wins}, Sussy: {sussy}, {gen_url}')
+            influencer = data['data']['userInfo']['isInfluencer']
+            country = data['data']['userInfo']['countryCode']
+            wins = data['data']['segments'][0]['stats']['wins']['value']
+            rewardlevel = data['data']['segments'][0]['stats']['seasonRewardLevel']['metadata']['rankName']
+            stats_totalprint = [wins, rewardlevel, influencer, premium, sussy, country, gen_url]
+            stats_totaldata = [wins, rewardlevel, influencer, premium, sussy, country, gen_url]
+
+            for i in range(0, len(data['data']['segments'])):
+                for x in range(1, 4):
+                    if f'{x}v{x}' in data['data']['segments'][i]['metadata']['name']:
+                        tier = data['data']['segments'][i]['stats']['tier']['metadata']['name']
+                        div = data['data']['segments'][i]['stats']['division']['metadata']['name']
+                        streak = data['data']['segments'][i]['stats']['winStreak']['displayValue']
+                        rankdict[f'{x}v{x}'] = f'{tier} {div} / {streak}'
+
+            for x in range(1, 4):
+                if f'{x}v{x}' not in rankdict.keys():
+                    rankdict[f'{x}v{x}'] = 'NULL'
+
+            sorted_rankdict = {k: v for k, v in sorted(rankdict.items())}
+            #print(sorted_rankdict)
+
+            totalprint.append(clr_name)
+            for key, value in sorted_rankdict.items():
+                totalprint.append(f'{rankdict[key]}')
+            for i in stats_totalprint:
+                totalprint.append(i)
+
+            table.append(totalprint)
+
+        print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
+
+        # Eventually, return list, where element 0 is an html table,
+        # and element 1 is the more detailed dictionary for database
+        # return []
 
     def main(self):
         updateWebdriver()
         while True:
-            stringy = self.readDatafile()
-            inDatadict = self.transformData(stringy)
+            stringy = self.readNames()
+            inDatadict = self.cleanNames(stringy)
             if inDatadict:
-                self.request(inDatadict)
+                self.handleData(inDatadict)
             sleep(15)
 
 
 if __name__ == '__main__':
     init = rl_playerinfo()
     print(init.main())
-
-
-
